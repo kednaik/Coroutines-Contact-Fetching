beebcf9ab853ff2bdf4c5a495bd2de57eefe611d
kedar <kedar@theinnerhour.com>
2020-04-20 20:07:20 +0530

    height changes of icon

--- app/src/main/java/com/kedar/coroutinescontactsfetching/ContactsViewModel.kt
+++ app/src/main/java/com/kedar/coroutinescontactsfetching/ContactsViewModel.kt
@@ -3,7 +3,6 @@ package com.kedar.coroutinescontactsfetching
 import android.app.Application
 import android.database.Cursor
 import android.provider.ContactsContract
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
